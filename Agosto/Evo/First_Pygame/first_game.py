9e5a3b4a3caa6707255770094702ae506520ad45
Tomas Lugo <ge52bar@mytum.de>
2019-08-19 16:20:52 -0700

    Adds linter and formatting settings

--- Agosto/Evo/First_Pygame/first_game.py
+++ Agosto/Evo/First_Pygame/first_game.py
@@ -22,21 +22,22 @@ pygame.display.set_caption(win_title)
 
 coordinate_1 = Coordinate.Coordinate(50, 50)
 dimension_1 = Dimensions.TwoDDimension(40, 60)
-start_and_dimension_1 = StartAndDimension.StartAndDimension(coordinate_1, dimension_1)
+start_and_dimension_1 = StartAndDimension.StartAndDimension(coordinate_1,
+                                                            dimension_1)
 
 color_1 = Color.RBGColor(255, 0, 0)
 
-character_1 = Character.Character(start_and_dimension_1, color_1, background_color, vel, win)
-#character_1.DrawItself(win)
+character_1 = Character.Character(
+    start_and_dimension_1, color_1, background_color, vel, win)
+# character_1.DrawItself(win)
 while win_life:
-	# Use actual timer later on
-	pygame.time.delay(delay_ms)
-	pygame.display.update()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			win_life = False
-	
-	
+    # Use actual timer later on
+    pygame.time.delay(delay_ms)
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            win_life = False
+
 
 pygame.display.update()
 pygame.time.delay(delay_ms)
