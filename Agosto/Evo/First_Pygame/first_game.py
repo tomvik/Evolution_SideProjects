da4c13195a123797f498a55b36494e03286185e0
Tomas Lugo <ge52bar@mytum.de>
2019-10-27 19:20:45 -0600

    -Uses pygame.Rect instead

--- Agosto/Evo/First_Pygame/first_game.py
+++ Agosto/Evo/First_Pygame/first_game.py
@@ -2,9 +2,6 @@ import pygame
 import random
 
 import Color
-import Coordinate
-import Dimensions
-import PositionAndDimension
 import Character
 import Collisions
 
@@ -12,41 +9,31 @@ pygame.init()
 
 delay_ms = 100
 background_color = Color.RBGColor(0, 0, 0)
-win_height = 700
+win_heigth = 700
 win_width = 1200
 win_title = "First game"
 win_life = True
 
-vel = 10
+vel = 5
 possible_movements = ["UP", "RIGHT", "DOWN", "LEFT"]
 number_of_characters = 2
 characters = []
 mov = []
 
-window_pd = PositionAndDimension.PositionAndDimension(
-    Coordinate.Coordinate(0, 0),
-    Dimensions.TwoDDimension(win_width, win_height))
-
-win = pygame.display.set_mode((win_width, win_height))
+win = pygame.display.set_mode((win_width, win_heigth))
 pygame.display.set_caption(win_title)
 
-coordinate_1 = Coordinate.Coordinate(50, 50)
-dimension_1 = Dimensions.TwoDDimension(10, 15)
-start_and_dimension_1 = PositionAndDimension.PositionAndDimension(coordinate_1,
-                                                                  dimension_1)
-
-coordinate_2 = Coordinate.Coordinate(120, 50)
-dimension_2 = Dimensions.TwoDDimension(10, 15)
-start_and_dimension_2 = PositionAndDimension.PositionAndDimension(coordinate_2,
-                                                                  dimension_2)
+window_rect = pygame.Rect(0, 0, win_width, win_heigth)
+rect_1 = pygame.Rect(50, 50, 10, 15)
+rect_2 = pygame.Rect(120, 50, 10, 15)
 
 color_1 = Color.RBGColor(255, 0, 0)
 color_2 = Color.RBGColor(0, 0, 255)
 
 characters.append(Character.Character(
-    start_and_dimension_1, color_1, background_color, vel, win))
+    rect_1, color_1, background_color, vel, win))
 characters.append(Character.Character(
-    start_and_dimension_2, color_2, background_color, vel, win))
+    rect_2, color_2, background_color, vel, win))
 
 for i in range(number_of_characters):
     mov.append(random.choice(possible_movements))
@@ -67,28 +54,21 @@ while win_life:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] \
-            and characters[0].is_move_possible("UP",
-                                               window_pd.get_vertex_limits()):
+            and characters[0].is_move_possible("UP", window_rect):
         characters[0].move("UP")
     elif keys[pygame.K_RIGHT] \
-            and characters[0].is_move_possible("RIGHT",
-                                               window_pd.get_vertex_limits()):
+            and characters[0].is_move_possible("RIGHT", window_rect):
         characters[0].move("RIGHT")
     elif keys[pygame.K_DOWN] \
-            and characters[0].is_move_possible("DOWN",
-                                               window_pd.get_vertex_limits()):
+            and characters[0].is_move_possible("DOWN", window_rect):
         characters[0].move("DOWN")
     elif keys[pygame.K_LEFT] \
-            and characters[0].is_move_possible("LEFT",
-                                               window_pd.get_vertex_limits()):
+            and characters[0].is_move_possible("LEFT", window_rect):
         characters[0].move("LEFT")
 '''
     for i in range(number_of_characters):
         mov[i] = random.choice(possible_movements)
-        while \
-                characters[i].is_move_possible(mov[i],
-                                               window_pd.get_vertex_limits()) \
-                is False:
+        while characters[i].is_move_possible(mov[i], window_rect) is False:
             mov[i] = random.choice(possible_movements)
         characters[i].move(mov[i])
 '''
