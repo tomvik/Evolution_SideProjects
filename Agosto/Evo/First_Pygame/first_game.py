43049c4ca25c126cca33eb867c97eb01a29f6e27
Tomas Lugo <ge52bar@mytum.de>
2019-10-28 07:24:40 -0600

    -Adds prototype of food -Creates function for random spanner inside arena

--- Agosto/Evo/First_Pygame/first_game.py
+++ Agosto/Evo/First_Pygame/first_game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from typing import List
 
 import Color
 import Rectangle
@@ -14,42 +15,47 @@ win_life = True
 
 vel = 5
 possible_movements = ["UP", "RIGHT", "DOWN", "LEFT"]
-number_of_characters = 2
-characters = []
-mov = []
+number_of_characters = 20
+number_of_foods = 20
+characters = list()
+mov = list()
 
 win = pygame.display.set_mode((win_width, win_heigth))
 pygame.display.set_caption(win_title)
 
-blocking_rects = []
+blocking_rects = list()
+stage = list()
+foods = list()
 window_rect = pygame.Rect(0, 0, win_width, win_heigth)
 stage_rects = (pygame.Rect(0, 0, 100, win_heigth),
                pygame.Rect(0, 0, win_width, 100),
                pygame.Rect(1100, 0, 100, win_heigth),
                pygame.Rect(0, 600, win_width, 100))
-rect_1 = pygame.Rect(150, 150, 10, 15)
-rect_2 = pygame.Rect(170, 150, 10, 15)
+actual_stage_rect = (pygame.Rect(100, 100, 1000, 500))
+rect_1 = pygame.Rect(0, 0, 10, 15)
+rect_2 = pygame.Rect(0, 0, 5, 5)
 
 color_1 = Color.RBGColor(255, 0, 0)
 color_2 = Color.RBGColor(0, 0, 255)
+food_color = Color.RBGColor(255, 255, 255)
 stage_color = Color.RBGColor(211, 211, 211)
 stage_walls_color = Color.RBGColor(0, 0, 0)
 
-characters.append(Rectangle.Rectangle(
-    rect_1, color_1, stage_color, win))
-blocking_rects.append(characters[-1])
-characters.append(Rectangle.Rectangle(
-    rect_2, color_2, stage_color, win))
-blocking_rects.append(characters[-1])
+stage.append(Rectangle.Rectangle(actual_stage_rect,
+                                 stage_color, stage_walls_color, win))
 
-stage = []
+characters = Rectangle.random_spanner(number_of_characters, actual_stage_rect,
+                                      rect_1, color_1, stage_color, win, True,
+                                      blocking_rects)
+
+foods = Rectangle.random_spanner(number_of_foods, actual_stage_rect, rect_2,
+                                 food_color, stage_color, win, False,
+                                 blocking_rects)
+
 for stage_rect in stage_rects:
     stage.append(Rectangle.Rectangle(
-        stage_rect, stage_color, stage_walls_color, win))
+        stage_rect, stage_walls_color, stage_walls_color, win))
     blocking_rects.append(stage[-1])
-
-for stage_rect in stage:
-    stage_rect.draw()
 
 for i in range(number_of_characters):
     mov.append(random.choice(possible_movements))
